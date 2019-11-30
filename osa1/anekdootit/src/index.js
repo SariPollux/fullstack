272214209903be0a23bc43c863af6ebab8ef295b
Sari Tähtinen <sari@magime-project.com>
2019-11-30 17:48:08 +0200

    tehty 1.13 anekdootit step2

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -5,13 +5,70 @@ const Button = ({ onClick, text}) => (
     <button onClick={onClick}>{text}</button>
 )
 
-const App = (props) => {
+const Display = ({anecdotes}) => {
+    return (
+    <div>{anecdotes}</div>
+    )
+}
+
+
+const Show = ({anecdotes, selected, votes}) => {
+    if (anecdotes[0]===anecdotes[selected]) {
+        return (
+            <div>has {votes[0]} votes</div>
+        )
+    }
+    else if (anecdotes[1]===anecdotes[selected]) {
+        return (
+            <div>has {votes[1]} votes</div>
+        )
+    }
+    else if (anecdotes[2]===anecdotes[selected]) {
+        return (
+            <div>has {votes[2]} votes</div>
+        )
+    }
+    else if (anecdotes[3]===anecdotes[selected]) {
+        return (
+            <div>has {votes[3]} votes</div>
+        )
+    }
+    else if (anecdotes[4]===anecdotes[selected]) {
+        return (
+            <div>has {votes[4]} votes</div>
+        )
+    }
+    else {
+        return (
+            <div>has {votes[5]} votes</div>
+        )
+    }
+}
+
+const App = ({anecdotes}) => {
 const [selected, setSelected] = useState(0)
+const [votes, setVotes] = useState(new Uint8Array(6))
+
+const copy = [...votes]
+if (anecdotes[0]===anecdotes[selected]) copy[0] += 1
+else if (anecdotes[1]===anecdotes[selected]) copy[1] += 1
+else if (anecdotes[2]===anecdotes[selected]) copy[2] += 1
+else if (anecdotes[3]===anecdotes[selected]) copy[3] += 1
+else if (anecdotes[4]===anecdotes[selected]) copy[4] += 1
+else copy[5] += 1
+
+console.log(votes)
+console.log(copy)
+
+
+
 
   return (
         <div>
-            <div>{props.anecdotes[selected]}</div>
-            <Button onClick={()=> setSelected(Math.floor(Math.random() * props.anecdotes.length))} text='next anecdote' />
+            <Display anecdotes = {anecdotes[selected]} />
+            <Show anecdotes = {anecdotes} selected = {selected} votes={votes} />
+            <button onClick={() => setVotes(copy)}>test</button>
+            <Button onClick={()=> setSelected(Math.floor(Math.random() * anecdotes.length))} text='next anecdote' />
         </div>
     )
 }
@@ -26,6 +83,5 @@ const anecdotes = [
 ]
 
 
-ReactDOM.render(<App anecdotes={anecdotes}/>, document.getElementById('root'));
-
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
 
