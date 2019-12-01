153fdf15ea12d3f17bf3c3c313e223f179208f1a
Sari Tähtinen <sari@magime-project.com>
2019-12-01 12:32:13 +0200

    tehtävä 1.14 valmis

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -5,12 +5,13 @@ const Button = ({ onClick, text}) => (
     <button onClick={onClick}>{text}</button>
 )
 
-const Display = ({anecdotes}) => {
+const Display = ({text}) => {
     return (
-    <div>{anecdotes}</div>
+    <div>{text}</div>
     )
 }
 
+const Header = ({text}) => <h1>{text}</h1>
 
 const Show = ({anecdotes, selected, votes}) => {
     if (anecdotes[0]===anecdotes[selected]) {
@@ -45,6 +46,39 @@ const Show = ({anecdotes, selected, votes}) => {
     }
 }
 
+const Favorite = ({anecdotes, most, votes}) => {
+    if (most===votes[0]) {
+        return (
+            <div>{anecdotes[0]}</div>
+        )
+    }
+    else if (most===votes[1]) {
+        return (
+            <div>{anecdotes[1]}</div>
+        )
+    }
+    else if (most===votes[2]) {
+        return (
+            <div>{anecdotes[2]}</div>
+        )
+    }
+    else if (most===votes[3]) {
+        return (
+            <div>{anecdotes[3]}</div>
+        )
+    }
+    else if (most===votes[4]) {
+        return (
+            <div>{anecdotes[4]}</div>
+        )
+    }
+    else {
+        return (
+            <div>{anecdotes[5]}</div>
+        )
+    }
+}
+
 const App = ({anecdotes}) => {
 const [selected, setSelected] = useState(0)
 const [votes, setVotes] = useState(new Uint8Array(6))
@@ -58,17 +92,22 @@ else if (anecdotes[4]===anecdotes[selected]) copy[4] += 1
 else copy[5] += 1
 
 console.log(votes)
-console.log(copy)
+//console.log(copy)
 
+const most = Math.max(...votes)
 
-
+console.log(most)
 
   return (
         <div>
-            <Display anecdotes = {anecdotes[selected]} />
+            <Header text = 'Anecdote of the day' />
+            <Display text = {anecdotes[selected]} />
             <Show anecdotes = {anecdotes} selected = {selected} votes={votes} />
-            <button onClick={() => setVotes(copy)}>test</button>
+            <Button onClick={() => setVotes(copy)} text='vote' />
             <Button onClick={()=> setSelected(Math.floor(Math.random() * anecdotes.length))} text='next anecdote' />
+            <Header text = 'Anecdote with most votes' />
+            <Favorite anecdotes = {anecdotes} most={most} votes={votes}/>
+            <Display text = {'has '+ (most) + ' votes'} />
         </div>
     )
 }
