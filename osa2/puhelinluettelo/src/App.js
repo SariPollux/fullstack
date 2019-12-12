1158007324f38dbaec86b6beb94fac870a4cb370
Sari Tähtinen <sari@magime-project.com>
2019-12-12 17:17:22 +0200

    Tehtävä 2.9: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,29 +1,39 @@
 import React, { useState } from 'react'
 
-const Person = ({person}) => {
-  return (
-    <div>{person.name} {person.number}</div>
-  )
-}
+const Person = ({person}) => (<div>{person.name} {person.number}</div>)
+
+const PersonsList = ({namesToShow}) => (namesToShow.map(person => <Person key={person.name} person={person} />))
+
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' } 
   ]) 
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ showFiltered, setShowFiltered ] = useState ('')
 
-  const showNames = () => persons.map(person =>
-  <Person
-  key={person.name}
-  person={person}
-  />
-  )
-  
+  /*const showNames = () => persons.map(person =>
+    <Person
+    key={person.name}
+    person={person}
+    />
+    )*/
+
+    /*const showNames = () => namesToShow.map(person =>
+      <Person
+      key={person.name}
+      person={person}
+      />
+      )*/
+
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.find(person => person.name === newName))
+    if (persons.find(({name}) => name === newName))
       {alert(`${newName} is already added to the phonebook`)
     } else {const nameObject = {
       name: newName,
@@ -34,17 +44,38 @@ const App = () => {
     setNewNumber('')}
   }
 
+  const namesToShow = persons.filter(person => {
+    return person.name.toLowerCase().includes(showFiltered.toLowerCase());
+  })
+
+ /*const namesToShow = persons.filter(({name}) => {
+    return name.toLowerCase().includes(showFiltered.toLowerCase());
+  })
+  console.log(namesToShow);*/
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+  const handleFilter = (event) => {
+    console.log(event.target.value);
+    setShowFiltered(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+    <div>
+      filter shown with <input
+                          value={showFiltered}
+                          onChange={handleFilter}
+                        />
+    </div>
+  </form>
+      <h3>add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -62,8 +93,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {showNames()}
+      <h3>Numbers</h3>
+      <PersonsList namesToShow={namesToShow} />
     </div>
   )
 
