6dc69fd42bcf494d5322fbe4c391c14e9abbe3a0
Sari Tähtinen <sari@magime-project.com>
2019-12-15 16:44:02 +0200

    aloitettu 2.12 maiden tiedot

---
+++ osa2/maat/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+/*const App = () => (
+    <div>
+        <p>Hello World</p>
+    </div>
+)*/
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
