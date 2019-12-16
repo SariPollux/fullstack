f08182ff6fa6ca8445f27f154097b7b596b4f421
Sari Tähtinen <sari@magime-project.com>
2019-12-16 17:21:49 +0200

    tehtävä 2.12 maiden teidot step1 jatkoa

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Country = ({country}) => {
+    return (<div>{country.name}</div>)
+}
+
+const CountryList = ({listCountry}) =>
+listCountry.map(country => 
+    <Country key={country.name}
+             country ={country}/>)
+
+const App = () => {
+
+const [countries, setCountries] = useState([])
+const [showFiltered, setShowFiltered] = useState('')
+
+useEffect(() => { 
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+})
+}, [])
+console.log(countries.length, );
 
 
-
-const App = () => (
-    <div>
-        <p>Hello World</p>
-    </div>
+const listCountry = countries.filter(country => {
+    return country.name.toLowerCase().includes(showFiltered.toLowerCase())
+}
 )
 
+
+
+const handleCountry = (event) => {
+        console.log(event.target.value);
+        setShowFiltered(event.target.value)
+      }
+return (
+    <div>
+    <form>
+    <div>
+        find countries <input
+                        value={showFiltered}
+                        onChange={handleCountry}
+                        />
+    </div>
+    </form>
+    <div><CountryList listCountry={listCountry} /></div>
+    </div>
+)
+}
 
 export default App
--- osa2/maat/src/index.js
+++ osa2/maat/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-/*const App = () => (
-    <div>
-        <p>Hello World</p>
-    </div>
-)*/
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
