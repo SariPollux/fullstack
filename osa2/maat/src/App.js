636701502e278b6c2fc84ecbabe5dda702cac0c9
Sari Tähtinen <sari@magime-project.com>
2019-12-17 19:11:39 +0200

    2.12 maiden tiedot step1 eteenpäin

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -5,50 +5,75 @@ const Country = ({country}) => {
     return (<div>{country.name}</div>)
 }
 
-const CountryList = ({listCountry}) =>
-listCountry.map(country => 
-    <Country key={country.name}
-             country ={country}/>)
 
-const App = () => {
+/*const Info = ({country}) => {
+    return (
+        <h1>{country.name}</h1>
+        <div>capital {capital}</div>
+        <div>population {population}</div>
+        <h2>languages</h2>
+    )
+}*/
 
-const [countries, setCountries] = useState([])
-const [showFiltered, setShowFiltered] = useState('')
 
-useEffect(() => { 
-    axios
+const CountryList = (props) => {
+        const {listCountry} = props
+        const pituus = listCountry.map(country=>{
+            return country.name
+        })
+        console.log(pituus);
+        if (pituus.length > 10) {
+            return (<div>Too many matches, specify another filter</div>)
+            
+        } else {
+            return (listCountry.map(country => {
+                return <Country key={country.name} country={country} />
+        }))}
+    }
+            
+    
+
+
+const App = (props) => {
+
+    const [countries, setCountries] = useState([])
+    const [showFiltered, setShowFiltered] = useState('')
+
+    useEffect(() => { 
+        axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
         setCountries(response.data)
-})
-}, [])
-console.log(countries.length, );
+        })
+    }, [])
+
+    
+
+   const listCountry = countries.filter(country => {
+        return country.name.toLowerCase().includes(showFiltered.toLowerCase())
+    })
+
+    console.log(listCountry);
 
 
-const listCountry = countries.filter(country => {
-    return country.name.toLowerCase().includes(showFiltered.toLowerCase())
-}
-)
-
-
-
-const handleCountry = (event) => {
+    const handleCountry = (event) => {
         console.log(event.target.value);
         setShowFiltered(event.target.value)
       }
-return (
-    <div>
-    <form>
-    <div>
-        find countries <input
+    return (
+        <div>
+        <form>
+        <div>
+            find countries <input
                         value={showFiltered}
                         onChange={handleCountry}
                         />
-    </div>
-    </form>
-    <div><CountryList listCountry={listCountry} /></div>
-    </div>
-)
+        </div>
+        </form>
+        <div><CountryList listCountry={listCountry}
+                         /></div>
+     </div>
+    )
 }
 
 export default App
